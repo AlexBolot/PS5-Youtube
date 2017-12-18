163ee3142d5e15abd728141681678788ce24c68e
Hugo Francois <hugo.francois@etu.unice.fr>
2017-12-18 08:54:13 +0100

    change project structure

---
+++ src/main/java/fr/uca/unice/polytech/si3/ps5/year17/engine/Main.java
@@ -0,0 +1,14 @@
+package fr.uca.unice.polytech.si3.ps5.year17.engine;
+
+/**
+ * @author Collet wa Mou Mosseiru
+ * @version 1.0
+ */
+
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+
+}
