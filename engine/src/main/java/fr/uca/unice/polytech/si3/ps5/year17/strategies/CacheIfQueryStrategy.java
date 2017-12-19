46a1ee5197101e42594daac6968d7cb8855176c3
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-12-19 08:31:02 +0100

    Implementing CacheIfQueryStrategie

--- engine/src/main/java/fr/uca/unice/polytech/si3/ps5/year17/strategies/CacheIfQueryStrategy.java
+++ engine/src/main/java/fr/uca/unice/polytech/si3/ps5/year17/strategies/CacheIfQueryStrategy.java
@@ -1,13 +1,10 @@
 package fr.uca.unice.polytech.si3.ps5.year17.strategies;
 
-import fr.uca.unice.polytech.si3.ps5.year17.Cache;
-import fr.uca.unice.polytech.si3.ps5.year17.Connexion;
-import fr.uca.unice.polytech.si3.ps5.year17.EndPoint;
+import fr.uca.unice.polytech.si3.ps5.year17.*;
 import fr.uca.unice.polytech.si3.ps5.year17.utils.ArrayList8;
 
 public class CacheIfQueryStrategy extends Strategy
 {
-
     public CacheIfQueryStrategy (ArrayList8<Connexion> connexions, ArrayList8<Cache> caches, ArrayList8<EndPoint> endPoints)
     {
         super(connexions, caches, endPoints);
@@ -15,6 +12,21 @@ public class CacheIfQueryStrategy extends Strategy
 
     public void apply ()
     {
+        for (EndPoint endPoint : endPoints)
+        {
+            for (Query query : endPoint.getQueries())
+            {
+                if (query.getNumberOfRequests() < 500) continue;
 
+                Video video = query.getVideo();
+
+                for (Connexion connexion : connexions.subList(connexion -> connexion.getIdEndPoint() == endPoint.getId()))
+                {
+                    Cache cache = caches.get(connexion.getIdCache());
+
+                    if (cache.addVideo(video)) break;
+                }
+            }
+        }
     }
 }
