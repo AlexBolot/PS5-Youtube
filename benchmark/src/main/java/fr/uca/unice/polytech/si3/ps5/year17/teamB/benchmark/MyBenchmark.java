40acbabedcf7bc8ea9e2678863cf4f824fd7c151
Hugo Francois <hugo.francois@etu.unice.fr>
2017-12-21 11:08:19 +0100

    add benchmark

--- benchmark/src/main/java/fr/uca/unice/polytech/si3/ps5/year17/teamB/benchmark/MyBenchmark.java
+++ benchmark/src/main/java/fr/uca/unice/polytech/si3/ps5/year17/teamB/benchmark/MyBenchmark.java
@@ -1,13 +1,45 @@
 package fr.uca.unice.polytech.si3.ps5.year17.teamB.benchmark;
 
+import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.Controller;
+import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.DataBundle;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.Main;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.Parser;
+import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.strategies.CacheIfQueryStrategy;
+import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.strategies.FirstInStrategy;
+import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.strategies.RandomStrategy;
+import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.strategies.Strategy;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
+@State(value = Scope.Benchmark)
 public class MyBenchmark {
 
+    private DataBundle data;
+    private Controller controller;
+    private String[] args;
+    private Parser parser;
+    private String kittensFile;
+    private Strategy random;
+    private Strategy firstIn;
+    private Strategy cacheIfQuery;
+
+
+    @Setup
+    public void setup() {
+        this.args = new String[]{"6",
+                "/home/doom/Documents/Git/Polytech/Collet_wa/engine/src/main/resources/me_at_the_zoo.in",
+                "/home/doom/Desktop/data.out",
+                "/home/doom/Desktop/score.out"};
+        this.kittensFile = "/home/doom/Documents/Git/Polytech/Collet_wa/engine/src/main/resources/kittens.in.txt";
+        this.parser = new Parser();
+        this.controller = new Controller(null);
+        parser.parse("/home/doom/Documents/Git/Polytech/Collet_wa/engine/src/main/resources/me_at_the_zoo.in");
+        this.data = parser.getData();
+        this.random = new RandomStrategy(data);
+        this.firstIn = new FirstInStrategy(data);
+        this.cacheIfQuery = new CacheIfQueryStrategy(data);
+    }
 
     @Benchmark
     @Fork(value = 2, warmups = 1)
@@ -15,19 +47,53 @@ public class MyBenchmark {
     @OutputTimeUnit(TimeUnit.SECONDS)
     @Threads(4)
     public void init() {
-        Main.main(new String[]{"6",
-                "/home/doom/Documents/Git/Polytech/Collet_wa/engine/src/main/resources/me_at_the_zoo.in",
-                "/home/doom/Desktop/data.out",
-                "/home/doom/Desktop/score.out"});
+        Main.main(args);
     }
 
+    @Benchmark
+    @Threads(4)
+    @Fork(value = 10, warmups = 2)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @BenchmarkMode(Mode.Throughput)
+    public void measureScoring() {
+        controller.scoring(data);
+    }
+
+    @Benchmark
+    @Threads(4)
+    @BenchmarkMode(Mode.Throughput)
+    @Fork(value = 10, warmups = 2)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void measureRandomStrategy() {
+        random.apply();
+        random = new RandomStrategy(data);
+    }
+
+    @Benchmark
+    @Threads(4)
+    @BenchmarkMode(Mode.Throughput)
+    @Fork(value = 10, warmups = 2)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void measureFirstInStrategy() {
+        firstIn.apply();
+        firstIn = new FirstInStrategy(data);
+    }
+
+    @Benchmark
+    @Threads(4)
+    @BenchmarkMode(Mode.Throughput)
+    @Fork(value = 10, warmups = 2)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void measureCacheIfQueryStrategy() {
+        cacheIfQuery.apply();
+        cacheIfQuery = new CacheIfQueryStrategy(data);
+    }
 
     @Benchmark
     @Fork(value = 10, warmups = 2)
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void measureParsingKittens() {
-        Parser parser = new Parser();
-        parser.parse("/home/doom/Documents/Git/Polytech/Collet_wa/engine/src/main/resources/kittens.in.txt");
+        parser.parse(kittensFile);
     }
 }
