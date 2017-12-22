e081b396f4f6d47711925ffd4c2c64812675cdac
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-12-22 14:33:44 +0100

    Try to upgrade CacheIfQuery :)

--- engine/src/main/java/fr/uca/unice/polytech/si3/ps5/year17/teamB/engine/strategies/CacheIfQueryStrategy.java
+++ engine/src/main/java/fr/uca/unice/polytech/si3/ps5/year17/teamB/engine/strategies/CacheIfQueryStrategy.java
@@ -3,29 +3,67 @@ package fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.strategies;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.*;
 
 /**
- * If there is more than 500 queries for a video, they are assigned to the cache that is
- * the nearest to the endpoint.
- * (A cache is nearer when there is less latency between it and the endpoint)
- */
-public class CacheIfQueryStrategy extends Strategy {
+ If there is more than 500 queries for a video, they are assigned to the cache that is
+ the nearest to the endpoint.
+ (A cache is nearer when there is less latency between it and the endpoint)
 
-    public CacheIfQueryStrategy(DataBundle data) {
+ <hr>
+ <h2>This is the Average Strategy :</h2>
+ <h3>If there is more than 500 queries for a video, they are assigned to the cache that is
+ the nearest to the endpoint.</h3>
+ <hr>
+ */
+public class CacheIfQueryStrategy extends Strategy
+{
+
+    /**
+     <hr>
+     <h2>Default Constructor of this Strategy<br>
+     <br>
+     See {@link Strategy#Strategy(DataBundle, int)}
+     </h2>
+     <hr>
+
+     @param data The DataBundle to apply this Strategy on.
+     */
+    public CacheIfQueryStrategy (DataBundle data)
+    {
         super(data, 6);
     }
 
-    public void apply() {
-        for (EndPoint endPoint : data.getEndPoints()) {
-            for (Query query : endPoint.getQueries()) {
-                if (query.getNumberOfRequests() < 500) continue;
+    public void apply ()
+    {
+        for (EndPoint endPoint : data.getEndPoints())
+        {
+            for (Query query : endPoint.getQueries())
+            {
+                if (query.getNumberOfRequests() < getAverageRequestAmount()) continue;
 
                 Video video = query.getVideo();
 
-                for (Connection connection : data.getConnections().subList(connexion -> connexion.getIdEndPoint() == endPoint.getId())) {
+                for (Connection connection : data.getConnections().subList(connexion -> connexion.getIdEndPoint() == endPoint.getId()))
+                {
                     Cache cache = data.getCaches().get(connection.getIdCache());
 
                     if (cache.addVideo(video)) break;
                 }
             }
         }
+    }
+
+    public int getAverageRequestAmount ()
+    {
+        int globalSum = 0;
+
+        if (data.getEndPoints().isEmpty()) return 0;
+
+        for (EndPoint endPoint : data.getEndPoints())
+        {
+            int localSum = endPoint.getQueries().stream().mapToInt(Query::getNumberOfRequests).sum();
+
+            globalSum += localSum / endPoint.getQueries().size();
+        }
+
+        return globalSum / data.getEndPoints().size();
     }
 }
--- visualizer/src/main/java/fr/uca/unice/polytech/si3/ps5/year17/teamB/visualizer/App.java
+++ visualizer/src/main/java/fr/uca/unice/polytech/si3/ps5/year17/teamB/visualizer/App.java
@@ -3,19 +3,38 @@ package fr.uca.unice.polytech.si3.ps5.year17.teamB.visualizer;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.Cache;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.DataBundle;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.Parser;
-import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.Video;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.utils.ArrayList8;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.visualizer.parser.OutputParser;
 
-import java.io.*;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 
 
-@SuppressWarnings("ResultOfMethodCallIgnored")
-public class App {
-    public static void main(String[] args) throws IOException {
+/**
+ <pre>
+ ||====================================================================||
+ ||//$\\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//$\\||
+ ||(100)==================| FEDERAL RESERVE NOTE |================(100)||
+ ||\\$//        ~         '------========--------'                \\$//||
+ ||<< /        /$\              // ____ \\                         \ >>||
+ ||>>|  12    //L\\            // ///..) \\         L38036133B   12 |<<||
+ ||<<|        \\ //           || <||  >\  ||                        |>>||
+ ||>>|         \$/            ||  $$ --/  ||        One Hundred     |<<||
+ ||<<|      L38036133B        *\\  |\_/  //* series                 |>>||
+ ||>>|  12                     *\\/___\_//*   1989                  |<<||
+ ||<<\      Treasurer     ______/Franklin\________     Secretary 12 />>||
+ ||//$\                 ~|UNITED STATES OF AMERICA|~               /$\\||
+ ||(100)===================  ONE HUNDRED DOLLARS =================(100)||
+ ||\\$//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\$//||
+ ||====================================================================||
+ </pre>
+ */
+@SuppressWarnings ("ResultOfMethodCallIgnored")
+public class App
+{
+    public static void main (String[] args) throws IOException
+    {
         DataBundle dataBundle = new Parser(args[0]).getData();
 
         String[] dataPaths = args[1].split(";");
@@ -26,42 +45,45 @@ public class App {
 
         OutputParser parser = new OutputParser();
 
-        for (String dataPath : dataPaths) {
+        for (String dataPath : dataPaths)
+        {
             listOfCaches.add(parser.parseDataOutput(dataPath));
         }
 
-        for (String scorePath : scorePaths) {
+        for (String scorePath : scorePaths)
+        {
             listOfScores.addAll(parser.parseScore(scorePath));
         }
 
         StringBuilder values = new StringBuilder();
 
-        for (int i = 0; i < listOfScores.size(); i++) {
-            values.append(listOfScores.get(i)).append(", ");
+        for (Float listOfScore : listOfScores)
+        {
+            values.append(listOfScore).append(", ");
         }
 
         values.deleteCharAt(values.length() - 1);
         values.deleteCharAt(values.length() - 1);
 
         StringBuilder str = new StringBuilder().append("myVals <- c(")
-                .append(values.toString())
-                .append(")\n")
-                .append("barplot(myVals,")
-                .append("main = \"Scoring by strategy\",\n")
-                .append("xlab = \"Strategies\",\n")
-                .append("ylab = \"Scoring\",\n")
-                .append("names.arg = c(")
-                .append("\"Average\",")
-                .append("\"CacheIfQuery\",")
-                .append("\"FirstIn\",")
-                .append("\"LightestsInCache\",")
-                .append("\"ProbaTegy\",")
-                .append("\"Random\"")
-                .append("),\n")
-                .append("col = \"darkred\",\n")
-                .append("horiz = FALSE,\n")
-                .append("cex.names = 0.75,\n")
-                .append("ylim = c(0,250000))");
+                                               .append(values.toString())
+                                               .append(")\n")
+                                               .append("barplot(myVals,")
+                                               .append("main = \"Scoring by strategy\",\n")
+                                               .append("xlab = \"Strategies\",\n")
+                                               .append("ylab = \"Scoring\",\n")
+                                               .append("names.arg = c(")
+                                               .append("\"Average\",")
+                                               .append("\"CacheIfQuery\",")
+                                               .append("\"FirstIn\",")
+                                               .append("\"LightestsInCache\",")
+                                               .append("\"ProbaTegy\",")
+                                               .append("\"Random\"")
+                                               .append("),\n")
+                                               .append("col = \"darkred\",\n")
+                                               .append("horiz = FALSE,\n")
+                                               .append("cex.names = 0.75,\n")
+                                               .append("ylim = c(0,250000))");
 
         File file = new File(args[args.length - 1] + "/graphN.r");
         if (!file.exists()) file.createNewFile();
@@ -70,11 +92,14 @@ public class App {
         printStream.print(str);
     }
 
-    private static float getPercentOfCovering(DataBundle dataBundle, ArrayList8<Cache> caches) {
+    private static float getPercentOfCovering (DataBundle dataBundle, ArrayList8<Cache> caches)
+    {
         float totalPercent = 0;
 
-        for (Cache realCache : dataBundle.getCaches()) {
-            for (Cache simuCache : caches) {
+        for (Cache realCache : dataBundle.getCaches())
+        {
+            for (Cache simuCache : caches)
+            {
                 if (realCache.getId() != simuCache.getId()) continue;
 
                 float initialSize = realCache.getSize();
