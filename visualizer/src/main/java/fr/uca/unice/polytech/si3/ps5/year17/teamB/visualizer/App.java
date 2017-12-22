fa6ae7337d6d87bf4bd9592b4a641e361b74530d
Camille Le Roux <camille.le-roux1@etu.unice.fr>
2017-12-22 16:37:51 +0100

    Graph Coverage

--- visualizer/src/main/java/fr/uca/unice/polytech/si3/ps5/year17/teamB/visualizer/App.java
+++ visualizer/src/main/java/fr/uca/unice/polytech/si3/ps5/year17/teamB/visualizer/App.java
@@ -3,6 +3,7 @@ package fr.uca.unice.polytech.si3.ps5.year17.teamB.visualizer;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.Cache;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.DataBundle;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.Parser;
+import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.Video;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.utils.ArrayList8;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.visualizer.parser.OutputParser;
 
@@ -85,16 +86,56 @@ public class App
                                                .append("cex.names = 0.75,\n")
                                                .append("ylim = c(0,250000))");
 
-        File file = new File(args[args.length - 1] + "/graphN.r");
+        File file = new File(args[args.length - 1] + "/graphScoring.r");
         if (!file.exists()) file.createNewFile();
 
         PrintStream printStream = new PrintStream(file.getPath());
         printStream.print(str);
+
+        StringBuilder values2 = new StringBuilder();
+
+        ArrayList8<Float> floatList = new ArrayList8<>();
+
+        for (ArrayList8<Cache> cas : listOfCaches) {
+            floatList.add(getPercentOfCovering(dataBundle,cas ));
+        }
+        for (Float listOfScore :floatList ) {
+            values2.append(listOfScore).append(", ");
+        }
+
+        values2.deleteCharAt(values2.length() - 1);
+        values2.deleteCharAt(values2.length() - 1);
+
+        StringBuilder str2 = new StringBuilder().append("myVals <- c(")
+                .append(values2.toString())
+                .append(")\n")
+                .append("barplot(myVals,")
+                .append("main = \"Coverage by strategy\",\n")
+                .append("xlab = \"Strategies\",\n")
+                .append("ylab = \"Coverage\",\n")
+                .append("names.arg = c(")
+                .append("\"Average\",")
+                .append("\"CacheIfQuery\",")
+                .append("\"FirstIn\",")
+                .append("\"LightestsInCache\",")
+                .append("\"ProbaTegy\",")
+                .append("\"Random\"")
+                .append("),\n")
+                .append("col = \"deeppink\",\n")
+                .append("horiz = FALSE,\n")
+                .append("cex.names = 0.75,\n")
+                .append("ylim = c(0,250000))");
+
+        File file2 = new File(args[args.length - 1] + "/graphCoverage.r");
+        if (!file2.exists()) file2.createNewFile();
+
+        PrintStream printStream2 = new PrintStream(file2.getPath());
+        printStream2.print(str2);
     }
 
     private static float getPercentOfCovering (DataBundle dataBundle, ArrayList8<Cache> caches)
     {
-        float totalPercent = 0;
+        double totalPercent = 0;
 
         for (Cache realCache : dataBundle.getCaches())
         {
@@ -102,16 +143,19 @@ public class App
             {
                 if (realCache.getId() != simuCache.getId()) continue;
 
-                float initialSize = realCache.getSize();
+                double initialSize = realCache.getSize();
 
-                simuCache.getVideos().forEach(realCache::addVideo);
+                for (Video simuVideo : simuCache.getVideos()) {
+                    Video video = dataBundle.getVideos().subList(video1 -> video1.getId() == simuVideo.getId()).get(0);
+                    realCache.addVideo(video);
+                }
 
-                float finalSize = realCache.getSize();
+                double finalSize = realCache.getSize();
 
-                totalPercent += (finalSize - initialSize) * 100 / finalSize;
+                totalPercent += (initialSize - finalSize) * 100 / finalSize;
             }
         }
 
-        return totalPercent / dataBundle.getCaches().size();
+        return (float) (totalPercent / dataBundle.getCaches().size());
     }
 }
