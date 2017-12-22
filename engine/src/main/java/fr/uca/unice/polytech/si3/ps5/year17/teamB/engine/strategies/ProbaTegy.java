10eb0290c5dd8a84e35ada5a3fa094cf9aa3c009
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-12-22 14:52:46 +0100

    Trying to upgrade ProbaTegy :)

--- engine/src/main/java/fr/uca/unice/polytech/si3/ps5/year17/teamB/engine/strategies/ProbaTegy.java
+++ engine/src/main/java/fr/uca/unice/polytech/si3/ps5/year17/teamB/engine/strategies/ProbaTegy.java
@@ -1,9 +1,9 @@
 package fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.strategies;
 
-import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.DataBundle;
-import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.utils.ArrayList8;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.Cache;
+import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.DataBundle;
 import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.Video;
+import fr.uca.unice.polytech.si3.ps5.year17.teamB.engine.utils.ArrayList8;
 
 import java.util.Comparator;
 import java.util.HashMap;
@@ -22,29 +22,46 @@ import static java.util.stream.Collectors.toMap;
  * |_____|   |_| |_|\__,_| \_/ \___|    \__,_|   |_.__/ \__,_|\__,_|   |_| \___|\___|_|_|_| |_|\__, |
  *                                                                                              __/ |
  *                                                                                             |___/
- *
  *        _                 _      _   _     _
  *       | |               | |    | | | |   (_)
  *   __ _| |__   ___  _   _| |_   | |_| |__  _ ___
  *  / _` | '_ \ / _ \| | | | __|  | __| '_ \| / __|
  * | (_| | |_) | (_) | |_| | |_   | |_| | | | \__ \  _ _ _
- * \__,_|_.__/ \___/ \__,_|\__|   \__|_| |_|_|___/ (_|_|_)
- *
- *
+ * \___,_|_.__/ \___/ \__,_|\__|   \__|_| |_|_|___/ (_|_|_)
  * </pre>
  *
- * Take the cache with the biggest number of connection and put in it all the video it can handle
- *
+ * <hr>
+ * <h2>ProbaTegy :</h2>
+ * <h3>Takes the cache with the biggest number of connection and put in it all the video it can handle</h3>
+ * <hr>
  */
-@SuppressWarnings("unchecked")
-public class ProbaTegy extends Strategy {
+@SuppressWarnings ("unchecked")
+public class ProbaTegy extends Strategy
+{
+    /**
+     <hr>
+     <h2>Default Constructor of this Strategy<br>
+     <br>
+     See {@link Strategy#Strategy(DataBundle, int)}
+     </h2>
+     <hr>
 
-    public ProbaTegy(DataBundle data) {
+     @param data The DataBundle to apply this Strategy on.
+     */
+    public ProbaTegy (DataBundle data)
+    {
         super(data, 1);
     }
 
+    /**
+     <hr>
+     <h2>Implementation of the apply method</h2>
+     <h3>Takes the cache with the biggest number of connection and put in it all the video it can handle</h3>
+     <hr>
+     */
     @Override
-    public void apply() {
+    public void apply ()
+    {
 
         HashMap<Integer, Integer> cachesConnections = new HashMap<>();
         ArrayList8<Video> cpy = new ArrayList8<>();
@@ -52,22 +69,32 @@ public class ProbaTegy extends Strategy {
         ArrayList8<Video> videoToRemove = new ArrayList8<>();
         Map<Integer, Integer> sortedCachesConnections;
 
-        for (Cache c : data.getCaches()) {
+        for (Cache c : data.getCaches())
+        {
             int nbConnexions = 0;
             nbConnexions += data.getConnections().stream().filter(co -> co.getIdCache() == c.getId()).count();
             cachesConnections.put(c.getId(), nbConnexions);
         }
 
         sortedCachesConnections = cachesConnections.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+                                                   .stream()
+                                                   .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                                                   .collect(toMap(Map.Entry::getKey,
+                                                                  Map.Entry::getValue,
+                                                                  (e1, e2) -> e1,
+                                                                  LinkedHashMap::new));
 
         data.getVideos().sort(Comparator.comparing(Video::getSize).reversed());
 
-        for (Integer cacheId : sortedCachesConnections.keySet()) {
-            for (Video video : cpy) {
-                if (data.getCaches().get(cacheId).getSize() >= video.getSize() && !data.getCaches().get(cacheId).getVideos().contains(video)) {
+        for (Integer cacheId : sortedCachesConnections.keySet())
+        {
+            for (Video video : cpy)
+            {
+                if (data.getCaches().get(cacheId).getSize() >= video.getSize() && !data.getCaches()
+                                                                                       .get(cacheId)
+                                                                                       .getVideos()
+                                                                                       .contains(video))
+                {
                     data.getCaches().get(cacheId).addVideo(video);
                     videoToRemove.add(video);
                 }
